a51c1c3a0b3bead362c3cb9952dd1ef80d4caf87
patriglezperez <patriciaggonzalezperez@hotmail.com>
2021-11-03 21:54:06 +0100

    add buttons to html and trying click js structure

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const test = test
+let game = 0; // A quien le toca poner
+
+let pieces = ['O', 'X'];
+
+let completedGame = false
+
+let movesNumber = 0; //Cuando sea 9 entonces completedGame = true
+
+
+// let addPiece = () => { console.log('Esta funcionando')}
+let addPiece = (btn) => {
+    btn.innerHTML = console.log(pieces[game])
+    movesNumber += 1;
+}
+
+let allButtons = document.querySelectorAll('button.boardButton')
+allButtons.forEach(ind => ind.addEventListener('click', addPiece))
+
+
+
