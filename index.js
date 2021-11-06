388275ab039a69774f99f7a3511004e94c589613
patriglezperez <patriciaggonzalezperez@hotmail.com>
2021-11-06 13:12:49 +0100

    x + o funcionality solved

--- index.js
+++ index.js
@@ -8,16 +8,25 @@ let movesNumber = 0; //Cuando sea 9 entonces completedGame = true
 
 
 // let addPiece = () => { console.log('Esta funcionando')}
-let addPiece = (btn) => {
-    game++;
-    //btn = console.log(game % 2 ? console.log(pieces[0]) : console.log(pieces[1]))
-    btn = document.getElementById('boarButton')
-    btn.innerHTML = game % 2 ? pieces[0] : pieces[1]
+let addPiece = (event) => {
+    event.preventDefault()
+   
+    let value = event.target.value
+    //console.log(game % 2 ? console.log(pieces[0]) : console.log(pieces[1]))
+   
+    let id =  event.target.id
+    
+    /* let innerTextButton = document.getElementsByClassName('boardButton')[0].innerHTML */
+    //console.log(innerTextButton)
+    let result = game % 2 ? pieces[0] : pieces [1] 
+    document.getElementById(id).innerHTML = result
+
+    game += 1;
+    
+    console.log('player '+ game)
 }
 
 
-let allButtons = document.querySelectorAll('button.boardButton')
-allButtons.forEach(ind => ind.addEventListener('click', addPiece))
 
 
 
