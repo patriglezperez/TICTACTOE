ba16505b93f2fc970fe11e1d7756a14f375fc68e
patriglezperez <patriciaggonzalezperez@hotmail.com>
2021-11-14 01:19:54 +0100

    change color or the pieces (with some problems)

--- index.js
+++ index.js
@@ -1,55 +1,50 @@
-let game = 0;
+/*let game = 0;
 let pieces = ['O', 'X'];
 let player_O = pieces[0];
 let player_X = pieces[1];
 
 let completedGame = false
+let movesNumber = 0;*/
+
+const buttons = Array.from(document.getElementsByClassName('boardButton'));
+
+const playerDisplay = document.querySelector('.display-player');
+const resetButton = document.querySelector('#reset');
+const announcer = document.querySelector('.announcer');
+
+let currentPlayer = 'X';
+
+let isGameActive = true;
+
+const PLAYERX_WON = 'PLAYERX_WON';
+const PLAYERO_WON = 'PLAYERO_WON';
+
 let movesNumber = 0;
+let won = false;
 
-let addPiece = (event) => {
+buttons.forEach(button => {
+    button.classList.remove(`player${currentPlayer}`)
+    button.classList.add(`player${currentPlayer}`)
+    currentPlayer = currentPlayer === 'O' ? 'X' : 'O';
+    //console.log(button.classList)
+})
 
-    let currentTurn = game % 2 ? player_O : player_X;
-
-    game++;
-
-    let button = event.target
-    if (!completedGame && button.innerHTML == "") {
-        button.innerHTML = currentTurn
-        /* let playerDisplay = document.getElementsById('player').innerHTML
-        if(currentTurn == 'X'){
-           playerDisplay.classList.add('playerX')
-        } else {
-            playerDisplay.classList.add('playerO')
-        } */
-        movesNumber++;
-    }
-
-    let id = event.target.id // id del boton pulsado
-    document.getElementById(id).disabled = true;
-
-    let nextTurn = game % 2 ? 'O' : 'X';
-     
-    document.getElementById('player').innerHTML = `${nextTurn}`
-    
-    if (validate()){
-        document.getElementById('winner').innerHTML += `¡¡ The winner is ${currentTurn} player !!`
-        if (currentTurn == "X" ) {
-    
-        }
-    } 
+function changePlayer() {
+    playerDisplay.classList.remove(`player${currentPlayer}`)
+    currentPlayer = currentPlayer === 'O' ? 'X' : 'O';
+    playerDisplay.innerHTML = currentPlayer;
+    playerDisplay.classList.add(`player${currentPlayer}`);
+    //console.log(currentPlayer)  
 }
 
-/* function Validate (...args) { //entran los botones
-    valueCell = args.map(ind => ind.innerHTML) // saco valo de los botones
-    if (valueCell[0] != "" && valueCell.for((i) => i == arr[0])){ // si todos los elementos son como el primer caso
-        args.forEach(n => n.style.backgroundColor = "Fuchsia")
-        return true
-    } else {
-        return false
-    }
-} */
-
-function validate() {
+function addPiece(event) {
+    let button = event.target
+    let id = event.target.id // id del boton pulsado
+    document.getElementById(id).disabled = true;
+    button.innerHTML = currentPlayer;
+    changePlayer();
+    movesNumber++;
+    console.log(movesNumber)
     let c0 = document.getElementById('0').innerHTML;
     let c1 = document.getElementById('1').innerHTML;
     let c2 = document.getElementById('2').innerHTML;
@@ -60,32 +55,61 @@ function validate() {
     let c7 = document.getElementById('7').innerHTML;
     let c8 = document.getElementById('8').innerHTML;
 
-    while (movesNumber >= 3) {
-        if ((c0 == c1) && (c0 == c2) && (c0)) {
-            //document.style.backgroundColor = 'Fuchsia'
-            return true;
-        } else if ((c3 == c4) && (c3 === c5) && (c5)) {
-            return true;
-        } else if ((c6 == c7) && (c6 === c8) && (c6)) {
-            return true;
-        } else if ((c0 == c3) && (c0 == c6) && (c0)) {
-            return true;
-        } else if ((c1 == c4) && (c1 == c7) && (c1)) {
-            return true;
-        } else if ((c2 == c5) && (c2 == c8) && (c2)) {
-            return true;
-        } else if ((c0 == c4) && (c0 == c8) && (c0)) {
-            return true;
-        }else if ((c2 == c4 ) && (c2 === c6) && (c2)){
-            return true;
-        } else {
-            return false;
+    if (button.innerHTML === '') {
+        won = false;
+    } else if ((c0 == c1) && (c0 == c2) && (c0)) {
+        //document.style.backgroundColor = 'Fuchsia'
+        won = true;
+    } else if ((c3 == c4) && (c3 == c5) && (c5)) {
+        won = true;
+    } else if ((c6 == c7) && (c6 == c8) && (c6)) {
+        won = true;
+    } else if ((c0 == c3) && (c0 == c6) && (c0)) {
+        won = true;
+    } else if ((c1 == c4) && (c1 == c7) && (c1)) {
+        won = true;
+    } else if ((c2 == c5) && (c2 == c8) && (c2)) {
+        won = true;
+    } else if ((c0 == c4) && (c0 == c8) && (c0)) {
+        won = true;
+    } else if ((c2 == c4) && (c2 === c6) && (c2)) {
+        won = true;
+    }
+
+
+    
+    const announce = (type) => {
+        switch (type) {
+            case PLAYERO_WON:
+                announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+                break;
+            case PLAYERX_WON:
+                announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+                break;
         }
-    } 
+        announcer.classList.remove('hide');
+    }
+    if ((movesNumber >= 3) && (won === true)) {
+        announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+        isGameActive = false;
+        return;
+    }
 }
 
-/* if (movesNumber > 9 || validate !=false){
-    console.log('fin del juego')
-} else {
-    console.log('sigue jugando')
-} */
+function reset() {
+
+    announcer.classList.add('hide');
+
+    if (currentPlayer === 'O') {
+        changePlayer();
+    }
+
+    buttons.forEach(button => {
+        button.innerText = '';
+        button.classList.remove('playerX');
+        button.classList.remove('playerO');
+    });
+    isGameActive = true;
+    addPiece()
+}
+
